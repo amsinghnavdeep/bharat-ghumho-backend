fd73820355fe733ee0d200b337fa6284f9c434b6
Nav Singh <amsinghnavdeep@gmail.com>
2026-05-07 15:17:15 -0400

    Add Netlify domain to CORS allowed origins

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://localhost:3000", "*"],
+    allow_origins=[
+        "http://localhost:4200",
+        "http://localhost:3000",
+        "https://bharat-ghumho.netlify.app",
+        "https://69fcbb1f99415800087d6e74--bharat-ghumho.netlify.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
